f8b8ed0d80fc756066539c2f761aac6c1afaf26d
MarcosGit2005 <marcosrubiodiaz@gmail.com>
2024-05-15 18:34:11 +0200

    20240515

---
+++ app/src/main/java/com/example/proyecto3ev_cliente/activities/model/Carrito.java
@@ -0,0 +1,10 @@
+package com.example.proyecto3ev_cliente.activities.model;
+
+
+import java.sql.Timestamp;
+
+public class Carrito {
+    private int idCarrito;
+    private String usuario;
+    private Timestamp changedTs;
+}
---
+++ app/src/main/java/com/example/proyecto3ev_cliente/activities/model/Contenido.java
@@ -0,0 +1,24 @@
+package com.example.proyecto3ev_cliente.activities.model;
+
+import java.sql.Timestamp;
+
+public class Contenido {
+    private int idContenido;
+    private String título;
+    private String género;
+    private String descripción;
+    private String imagen;
+    private String idioma;
+    private double precio;
+    private double valoraciónMedia;
+    private String nombre_director;
+    private int duración;
+    private String actoresPrincipales;
+    private String fechaEstreno;
+    private String tipoContenido;
+    private Timestamp changedTs;
+    private String disponibleHasta;
+    private String nombreSerie;
+    private int numeroTemporada;
+    private boolean disponibilidad;
+}
